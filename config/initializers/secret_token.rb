4f2ba8b7cfa795fe23ed5d44f7382d2483afab5d
Cameron Harris <cameroniharris1@gmail.com>
2015-10-25 21:17:09 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stopwatch::Application.config.secret_key_base = '75591e554f9306e62b96347ee31cd97e83a0efe91b4d345bd7c5613cf4fcb992985f9cc625bba001399220e6b8c88f10f9fb9b49b3d9dcb8f0be3eae592e0ae4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stopwatch::Application.config.session_store :cookie_store, key: '_stopwatch_session'
